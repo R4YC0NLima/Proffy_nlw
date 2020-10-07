ffb780c291ffab6f817098a24793213f3a19481d
Raycon <rayconlimabatista@gmail.com>
2020-10-07 09:35:51 -0400

    passou!

--- backend/knexfile.ts
+++ backend/knexfile.ts
@@ -1,12 +1,14 @@
 import path from 'path';
+import 'dotenv/config' 
 
 module.exports = {
     client: 'mysql',
     connection: {
-        user : 'root',
-        host: 'mysql_proffy',
-        password : 'someRandomPassword',
-        database : 'nlw_proffy'
+        host : process.env.DB_HOST,
+        port : process.env.DB_PORT,
+        user : process.env.DB_USERNAME,
+        password : process.env.DB_PASSWORD,
+        database : process.env.DB_DATABASE,
     },
     migrations: {
         directory: path.resolve(__dirname, 'src', 'database', 'migrations')
