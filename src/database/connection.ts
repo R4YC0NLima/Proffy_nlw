29558d42a93342fde02bb2989a59abfbdaec672b
Raycon <rayconlimabatista@gmail.com>
2020-10-07 09:35:51 -0400

    passou!

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,12 +1,14 @@
 import knex from 'knex';
+import 'dotenv/config' 
 
 const db = knex({
     client: 'mysql',
     connection: {
-        user: 'root',
-        host: 'mysql_proffy',
-        password: 'someRandomPassword',
-        database: 'nlw_proffy'
+        host : process.env.DB_HOST,
+        port : process.env.DB_PORT,
+        user : process.env.DB_USERNAME,
+        password : process.env.DB_PASSWORD,
+        database : process.env.DB_DATABASE
     }
 })
 
